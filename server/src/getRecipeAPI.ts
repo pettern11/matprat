95200ef064a9d95584f74be5e22fe034096dd487
martishv <martishv@stud.ntnu.no>
2022-10-20 15:24:08 +0200

    script som henter oppskrifter fra db

--- server/src/getRecipeAPI.ts
+++ server/src/getRecipeAPI.ts
@@ -1,39 +1,103 @@
-// import pool from './mysql-pool';
-// import type { RowDataPacket, ResultSetHeader } from 'mysql2';
-// import { Country, Category, Ingredient, Recipe, Recipe_Content } from './services';
+import mysql from 'mysql2';
+/*
+getRecipeAPI.ts bruker ES-Modules, og krever ES5/6, og ikke commonJS.
+Node forventer commonjs, men babel-node kan oversette es-moduler, vi bruker derfor babel-node for å kjøre scriptet.
 
-// function getIngredientAPI() {
-//   fetch('www.themealdb.com/api/json/v1/1/list.php?i=list')
-//     .then((res) => res.json())
-//     .then((data) => console.log(data.meals));
-// }
-// function getCountryAPI() {
-//   fetch('www.themealdb.com/api/json/v1/1/list.php?a=list')
-//     .then((res) => res.json())
-//     .then((data) => console.log(data.meals));
-// }
-let allRecipes = [];
-let navn = [];
+babel-node skal i teorien fungere alene, men fant ut det ikke kjører uten nodemon foran. 
+Enkleste løsning er å bruke nodemon, og manuelt avslutte programmet etter det har kjørt. */
+const pool = mysql.createPool({
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+  // Reduce load on NTNU MySQL server
+  connectionLimit: 1,
+  // Convert MySQL boolean values to JavaScript boolean values
+  typeCast: (field, next) =>
+    field.type == 'TINY' && field.length == 1 ? field.string() == '1' : next(),
+});
+
+let recipe = [];
+let alfabeth1 = ['a', 'b', 'c'];
+let alfabeth2 = ['d', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm'];
+let alfabeth3 = ['n', 'o', 'p', 'r', 's', 't', 'v', 'w', 'y'];
+
 function getRecipeAPI() {
-  fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=')
-    .then((res) => res.json())
-    // .then((data) => console.log(data.meals.length));
-    .then((data) =>
-      data.meals.forEach((element) => {
-        allRecipes.push(element);
-        // console.log(element);
-      })
-    )
-    .then(() =>
-      allRecipes.forEach((element) => {
-        navn.push(element.strMeal);
-      })
-    );
+  alfabeth1.forEach((letter) => {
+    fetch('https://www.themealdb.com/api/json/v1/1/search.php?f=' + letter)
+      .then((res) => res.json())
+      // .then((data) => console.log(data.meals.length));
+      .then((data) => aaaa1(data.meals));
+  });
+  alfabeth2.forEach((letter) => {
+    fetch('https://www.themealdb.com/api/json/v1/1/search.php?f=' + letter)
+      .then((res) => res.json())
+      // .then((data) => console.log(data.meals.length));
+      .then((data) => aaaa2(data.meals));
+  });
+  alfabeth3.forEach((letter) => {
+    fetch('https://www.themealdb.com/api/json/v1/1/search.php?f=' + letter)
+      .then((res) => res.json())
+      // .then((data) => console.log(data.meals.length));
+      .then((data) => aaaa3(data.meals));
+  });
 }
 
-// getIngredientAPI();
-// getCountryAPI();
+function aaaa1(array) {
+  array.forEach((element) => {
+    if (!recipe.includes(element.idMeal)) {
+      recipe.push({
+        id: element.idMeal,
+        name: element.strMeal,
+        instruction: element.strInstructions,
+        picture: element.strMealThumb,
+        country: element.strArea,
+        category: element.strCategory,
+      });
+    }
+  });
+}
+function aaaa2(array) {
+  array.forEach((element) => {
+    if (!recipe.includes(element.idMeal)) {
+      recipe.push({
+        id: element.idMeal,
+        name: element.strMeal,
+        instruction: element.strInstructions,
+        picture: element.strMealThumb,
+        country: element.strArea,
+        category: element.strCategory,
+      });
+    }
+  });
+}
+function aaaa3(array) {
+  array.forEach((element) => {
+    if (!recipe.includes(element.idMeal)) {
+      recipe.push({
+        id: element.idMeal,
+        name: element.strMeal,
+        instruction: element.strInstructions,
+        picture: element.strMealThumb,
+        country: element.strArea,
+        category: element.strCategory,
+      });
+    }
+  });
+}
+
 getRecipeAPI();
+
 setTimeout(() => {
-  console.log(navn);
-}, 1000);
+  recipe.forEach((element) => {
+    pool.query(
+      'INSERT INTO oppskrift SET oppskrift_id=?, oppskrift_navn=?, oppskrift_beskrivelse=?, oppskrift_steg=?,ant_pors=?,bilde_adr=?,kategori_id=?,land_id=?,ant_like=?',
+      [element.id, element.name, '', element.instruction, 4, element.picture, 1, 1, 0],
+      (error, results) => {
+        if (error) return error;
+
+        results;
+      }
+    );
+  });
+}, 2000);
